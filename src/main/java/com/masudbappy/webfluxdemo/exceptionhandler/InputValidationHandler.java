38e223893a8610b57a5aa761658140384b2731c3
masudbappy <masud.bappy90@gmail.com>
2022-12-01 22:19:16 +0600

    Added Exception Handler

---
+++ src/main/java/com/masudbappy/webfluxdemo/exceptionhandler/InputValidationHandler.java
@@ -0,0 +1,21 @@
+package com.masudbappy.webfluxdemo.exceptionhandler;
+
+import com.fasterxml.jackson.core.exc.InputCoercionException;
+import com.masudbappy.webfluxdemo.dto.InputFailedValidationResponse;
+import com.masudbappy.webfluxdemo.exception.InputValidationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class InputValidationHandler {
+
+    @ExceptionHandler(InputValidationException.class)
+    public ResponseEntity<InputFailedValidationResponse> handleException(InputValidationException ex) {
+        InputFailedValidationResponse response = new InputFailedValidationResponse();
+        response.setInput(ex.getInput());
+        response.setMessage(ex.getMessage());
+        response.setErrorCode(ex.getErrorCode());
+        return ResponseEntity.badRequest().body(response);
+    }
+}
